a51b87a3cb92489e57623eabe4d94616441851d2
Yoni Gang <yonigang1@gmail.com>
2024-03-14 19:18:32 +0200

    add models

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from sqlalchemy import JSON
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -13,8 +15,21 @@ class City(Base):
 
     settlement_symbol: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[dict] = mapped_column(JSON, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
 
 
 class PopulationByAge(Base):
     __tablename__ = "population_by_age"
     __mapper_args__ = {"eager_defaults": True}
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    city: Mapped['City'] = relationship("City", back_populates="population_by_age",
+                                        lazy="selectin")
+    data_date: Mapped[datetime] = mapped_column(index=True, nullable=False)
+    range_0_5: Mapped[int]
+    range_6_18: Mapped[int]
+    range_19_45: Mapped[int]
+    range_46_55: Mapped[int]
+    range_56_64: Mapped[int]
+    range_65_and_more: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
